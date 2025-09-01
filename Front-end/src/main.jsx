41dd31b7d43b7d4949fc33dd91fa74622f2e9685
Balaji007k <k446521@gmail.com>
2025-09-01 16:34:22 +0530

    small changes

--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 import { ThemeProvider } from './ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <ThemeProvider>
-      <BrowserRouter basename="/Teabuff/">
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+  <ThemeProvider>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </ThemeProvider>
 );
