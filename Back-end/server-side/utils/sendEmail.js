3f4fb2c1a39e2cd2ff9af7008ecd4f7325c985f3
Balaji007k <k446521@gmail.com>
2025-09-24 16:28:27 +0530

    gitignore added

--- Back-end/server-side/utils/sendEmail.js
+++ Back-end/server-side/utils/sendEmail.js
@@ -1,31 +1,62 @@
+// const env = require('dotenv');
+// const path = require('path');
+// const nodemailer = require("nodemailer");
+
+// env.config({ path: path.resolve(__dirname, '../config.env') }); 
+
+// if (!process.env.SENDGRID_API_KEY) {
+//   throw new Error(".env variable SENDGRID_API_KEY is missing!");
+// }
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.sendgrid.net",
+//   port: 587,
+//   auth: {
+//     user: "apikey",                   // literally "apikey"
+//     pass: process.env.SENDGRID_API_KEY,
+//   },
+// });
+
+// const sendOrderConfirmation = async (to, subject, text) => {
+//   try {
+//     const info = await transporter.sendMail({
+//       from: `"Teabuff Store" <${process.env.EMAIL_USER}>`, // verified in SendGrid
+//       to,
+//       subject,
+//       text,
+//     });
+//     console.log("Email sent:", info.messageId);
+//     return true;
+//   } catch (err) {
+//     console.error("Error sending email:", err);
+//     return false;
+//   }
+// };
+
+// module.exports = { sendOrderConfirmation };
+
 const env = require('dotenv');
 const path = require('path');
-const nodemailer = require("nodemailer");
+const sgMail = require('@sendgrid/mail');
 
-env.config({ path: path.resolve(__dirname, '../config.env') }); 
+env.config({ path: path.resolve(__dirname, '../config.env') });
 
 if (!process.env.SENDGRID_API_KEY) {
   throw new Error(".env variable SENDGRID_API_KEY is missing!");
 }
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.sendgrid.net",
-  port: 587,
-  auth: {
-    user: "apikey",                   // literally "apikey"
-    pass: process.env.SENDGRID_API_KEY,
-  },
-});
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendOrderConfirmation = async (to, subject, text) => {
   try {
-    const info = await transporter.sendMail({
+    const msg = {
+      to, // recipient email
       from: `"Teabuff Store" <${process.env.EMAIL_USER}>`, // verified in SendGrid
-      to,
       subject,
       text,
-    });
-    console.log("Email sent:", info.messageId);
+    };
+    const response = await sgMail.send(msg);
+    console.log("Email sent:", response[0].statusCode);
     return true;
   } catch (err) {
     console.error("Error sending email:", err);
